92460c36836203428682315afcc4192c24355092
Gabe Jensen <thegabejensen@gmail.com>
2023-03-30 06:42:04 -0700

    Got rid of old rust toolchain rust version, adding in sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-#[cfg(test)] mod tests;
+#[cfg(test)]
+mod tests;
 
 #[get("/")]
+fn index() -> &'static str {
+    "It works!"
+}
+
+#[get("/hello")]
 fn hello() -> &'static str {
     "Hello, world!"
 }
 
+// #[get("/data")]
+// fn select_something() -> String {
+
+// }
+
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite().mount("/", routes![index, hello]).launch();
+    println!("HELLO!");
 }
