b63104d7bdf5d41d827f1181ab87860f9117ee9d
Gabe Jensen <thegabejensen@gmail.com>
2023-04-01 12:59:04 -0700

    Got it connecting to the database

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,68 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use]
 extern crate rocket;
 extern crate dotenv;
 
 use dotenv::dotenv;
+use rocket::serde::json::Json;
+use rocket::serde::Serialize;
+use rocket::State;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{FromRow, PgPool};
 use std::env;
 
-#[cfg(test)]
-mod tests;
+#[derive(Debug, FromRow, Serialize)]
+struct User {
+    id: i32,
+    name: String,
+}
+struct GabeTest {
+    pool: PgPool,
+}
 
 #[get("/")]
 fn index() -> &'static str {
-    "It works!"
-}
-
-#[get("/hello")]
-fn hello() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/environment")]
-fn environment() -> String {
-    env::var("RUST_ENV").unwrap_or("yikes".to_string())
+#[get("/test")]
+async fn test(gabe: &State<GabeTest>) -> Json<Option<User>> {
+    let user = sqlx::query_as::<_, User>("select id, name from users where id=$1")
+        .bind(10)
+        .fetch_one(&gabe.pool)
+        .await;
+
+    Json(Some(user.unwrap()))
 }
 
-fn main() {
+#[get("/test2/<id>")]
+async fn test2(id: i32, gabe: &State<GabeTest>) -> Json<Option<User>> {
+    let user = sqlx::query_as::<_, User>("select id, name from users where id=$1")
+        .bind(id)
+        .fetch_one(&gabe.pool)
+        .await;
+
+    match user {
+        Ok(u) => Json(Some(u)),
+        Err(e) => {
+            dbg!(e);
+            Json(None)
+        }
+    }
+}
+
+#[launch]
+async fn rocket() -> _ {
     dotenv().ok();
 
-    let app_env = env::var("RUST_ENV").unwrap_or("yikes".to_string());
-    dbg!(app_env);
+    let db_url = env::var("DATABASE_URL").unwrap();
 
-    rocket::ignite()
-        .mount("/", routes![index, hello, environment])
-        .launch();
+    let pool = PgPoolOptions::new()
+        .max_connections(100)
+        .connect(&db_url)
+        .await
+        .expect("Unable to connect to Postgres");
+
+    rocket::build()
+        .manage(GabeTest { pool })
+        .mount("/", routes![index, test, test2])
 }
